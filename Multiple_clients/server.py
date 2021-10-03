b78db7bc96841c7aadaa63cf8bda91d931068b78
Raswanth Rajangam <59132948+Raswanth8@users.noreply.github.com>
2021-10-03 12:37:56 +0530

    Update server.py

--- Multiple_clients/server.py
+++ Multiple_clients/server.py
@@ -14,7 +14,7 @@ def threaded(c,addr):
             print_lock.release()
             break
         
-        #data = open("E:/Amrita University/Academics/3rd year 2019-20/6th Semester/Computer Networks/Lab/Lab 3/data.txt","r")
+        
         data = "To Client "+ str(addr[1])
         c.send(str(data).encode())
     c.close()
@@ -26,7 +26,7 @@ def main():
     s.bind((host,port))
     s.listen(5)
 
-    # data = pd.read_csv("E:/Amrita University/Academics/3rd year 2019-20/6th Semester/Computer Networks/Lab/Lab 4/data.csv")
+    
     print("Server Running.... ")
     while True:
         c,addr = s.accept()
